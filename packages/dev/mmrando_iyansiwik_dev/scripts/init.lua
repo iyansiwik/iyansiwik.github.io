00408c47fddc7e7f84fe179e15204181965a9181
Iyan Siwik <iyanazksiwik@gmail.com>
2019-02-23 15:52:21 -0500

    Continued working on map tracker

--- packages/dev/mmrando_iyansiwik_dev/scripts/init.lua
+++ packages/dev/mmrando_iyansiwik_dev/scripts/init.lua
@@ -1,13 +1,24 @@
+Tracker:AddItems("items/items.json")
+Tracker:AddItems("items/masks.json")
+Tracker:AddItems("items/songs.json")
+Tracker:AddItems("items/trade_items.json")
 Tracker:AddItems("items/dungeon_items.json")
+Tracker:AddItems("items/map_settings.json")
+Tracker:AddItems("items/capture_items.json")
 
 Tracker:AddMaps("maps/maps.json")
 
 ScriptHost:LoadScript("scripts/tingle.lua")
 
-Tracker:AddLocations("locations/termina/clocktown.json")
-Tracker:AddLocations("locations/termina/termina.json")
-Tracker:AddLocations("locations/termina/tingle.json")
+Tracker:AddLocations("locations/tingle.json")
+Tracker:AddLocations("locations/keaton.json")
+
+Tracker:AddLocations("locations/clocktown.json")
+
+Tracker:AddLocations("locations/termina/gossips.json")
+Tracker:AddLocations("locations/termina/south.json")
 
 Tracker:AddLayouts("layouts/shared.json")
 Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/capture.json")
 Tracker:AddLayouts("layouts/broadcast.json")
