77e4e31c5af1e1905242512e820cd3f095daf4e1
Iyan Siwik <iyanazksiwik@gmail.com>
2021-04-11 19:55:24 -0400

    test 3

--- hivestuck/story/saveloadgame.js
+++ hivestuck/story/saveloadgame.js
@@ -1,6 +1,9 @@
 window.onload = function() {
+	window.alert("sometext");
+	
 	var save = document.getElementById("saveLink");
 	a.onclick = function(page) {
+		window.alert("save");
 		window.localStorage.setItem('hivestuck_page', page);
 		
 		return false;
@@ -8,12 +11,14 @@ window.onload = function() {
 	
 	var load = document.getElementById("loadLink");
 	a.onclick = function() {
+		window.alert("load");
 		page = window.localStorage.getItem('hivestuck_page');
 		window.location.assign("https://www.google.com");
 	}
 	
 	var auto = document.getElementById("autoLink");
 	a.onclick = function() {
+		window.alert("auto");
 		window.localStorage.setItem('hivestuck_auto', 1);
 		
 		return false;
@@ -21,6 +26,7 @@ window.onload = function() {
 	
 	var del = document.getElementById("delLink");
 	a.onclick = function() {
+		window.alert("del");
 		window.localStorage.setItem('hivestuck_page', 0);
 		window.localStorage.setItem('hivestuck_auto', 0);
 		
